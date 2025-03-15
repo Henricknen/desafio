62a99fe3e88b1978e52fbb7be6208221d94db2f0
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-03-15 19:53:14 -0300

    corrigindo_refazendo_desafio_'lista_de_tarefas'

--- lista_de_tarefas/js/script.js
+++ lista_de_tarefas/js/script.js
@@ -1,110 +1,101 @@
-// seleção dos elementos
-const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");      // selecionando os elementos
 const todoInput = document.querySelector("#todo-input");
+const priorityInput = document.querySelector("#priority-input");
 const todoList = document.querySelector("#todo-list");
-const editForm = document.querySelector("#edit-form");
-const editInput = document.querySelector("#edit-input");
-const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-let oldInputValue;
+let todos = [];
 
-// funções
-const saveTodo =(text)=> {
-    const todo = document.createElement("div");     // criando 'div' geral e armazenando na constante 'todo'
-    todo.classList.add("todo");     // adiçionando a 'classe' todo
-
-    const todoTitle = document.createElement("h3");
-    todoTitle.innerText = text;
-    todo.appendChild(todoTitle);
-
-    const doneBtn = document.createElement("button");
-    doneBtn.classList.add("finish-todo");
-    doneBtn.innerHTML = '<i class = "fa fa-solid fa-check"></i>';
-    todo.appendChild(doneBtn);
-
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-todo");
-    editBtn.innerHTML = '<i class = "fa fa-solid fa-pen"></i>';
-    todo.appendChild(editBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("remove-todo");
-    deleteBtn.innerHTML = '<i class = "fa fa-solid fa-xmark"></i>';
-    todo.appendChild(deleteBtn);
-
-    todoList.appendChild(todo);     // inserindo na lista geral
-    todoInput.value = "";
-    todoInput.focus();
+const saveTodo = (description, priority) => {       // função que salva as tarefas
+    const todo = {
+        id: Date.now(),
+        description,
+        priority,
+        status: 'pendente'      // status inicial
+    };
+    todos.push(todo);
+    renderTodos();
 };
 
-const toggleForms =()=> {
-    editForm.classList.toggle("hide");      // 'hide' exibe o escondo editForm
-    todoForm.classList.toggle("hide");
-    todoList.classList.toggle("hide");
-};
+const renderTodos = () => {     // função que renderizar as tarefas
+    todoList.innerHTML = '';
+    todos.forEach(todo => {
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        todoDiv.setAttribute("data-id", todo.id);
 
-const updateTodo =(text)=> {
-    const todos = document.querySelectorAll(".todo");
-
-    todos.forEach((todo)=> {        // percorrendo o array 
-        let todoTitle = todo.querySelector("h3");       // pegando o titulo da tarefa
-        
-        if(todoTitle.innerText === oldInputValue) {
-            todoTitle.innerText = text;
+        const todoTitle = document.createElement("h3");
+        todoTitle.innerText = `${todo.description} - Prioridade: ${todo.priority} - Status: ${todo.status}`;
+        if (todo.status === 'concluída') {
+            todoTitle.classList.add("done");        // adicionando a classe 'done' se a tarefa estiver concluída
         }
+        todoDiv.appendChild(todoTitle);
+
+        const doneBtn = document.createElement("button");
+        doneBtn.classList.add("finish-todo");
+        doneBtn.innerHTML = '<i class="fa fa-check"></i>';
+        todoDiv.appendChild(doneBtn);
+
+        const editBtn = document.createElement("button");
+        editBtn.classList.add("edit-todo");
+        editBtn.innerHTML = '<i class="fa fa-pen"></i>';
+        todoDiv.appendChild(editBtn);
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("remove-todo");
+        deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
+        todoDiv.appendChild(deleteBtn);
+
+        todoList.appendChild(todoDiv);
     });
 };
 
+const toggleTodoStatus = (id) => {      // função que atualiza o status da tarefa
+    const todo = todos.find(todo => todo.id === id);
+    if (todo) {
+        todo.status = todo.status === 'pendente' ? 'concluída' : 'pendente';        // alterna o status
+        renderTodos();      // atualiza a visualização
+    }
+};
+
+const editTodo = (id) => {      // função que edita a tarefa
+    const todo = todos.find(todo => todo.id === id);
+    if (todo) {
+        todoInput.value = todo.description;
+        priorityInput.value = todo.priority;
+        deleteTodo(id);     // remove a tarefa original para edição
+    }
+};
+
+const deleteTodo = (id) => {        // função que remove a tarefa
+    todos = todos.filter(todo => todo.id !== id);
+    renderTodos();
+};
+
 // eventos
-todoForm.addEventListener("submit", (e)=> {      // evento de envio
-    e.preventDefault();     // evita que o formúlario seja enviado ao precionar o botão
-
-    const inputValue = todoInput.value;     // pegando o valor do input para criar nova tarefa
-    
-    if(inputValue) {
-        saveTodo(inputValue);
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const description = todoInput.value;
+    const priority = priorityInput.value;
+    if (description) {
+        saveTodo(description, priority);
+        todoInput.value = '';
     }
 });
 
-document.addEventListener("click", (e)=> {    
+document.addEventListener("click", (e) => {
     const targetEl = e.target;
-    const parentEl = targetEl.closest(".todo");        // seleçionando o elemento pai
-    let todoTitle;
+    const parentEl = targetEl.closest(".todo");
+    const todoId = parentEl ? parseInt(parentEl.getAttribute("data-id")) : null;
 
-    if(parentEl && parentEl.querySelector("h3")) {
-        todoTitle = parentEl.querySelector("h3").innerText;
+    if (targetEl.classList.contains("finish-todo")) {
+        toggleTodoStatus(todoId);
     }
 
-    if(targetEl.classList.contains("finish-todo")) {
-        parentEl.classList.toggle("done");      // 'adiçionando' a classe done nas tarefas clicadas
+    if (targetEl.classList.contains("edit-todo")) {
+        editTodo(todoId);
     }
 
-    if(targetEl.classList.contains("remove-todo")) {
-        parentEl.remove();      // removendo o elemento pai
+    if (targetEl.classList.contains("remove-todo")) {
+        deleteTodo(todoId);
     }
-
-    if(targetEl.classList.contains("edit-todo")) {
-        toggleForms();
-
-        editInput.value = todoTitle;
-        oldInputValue = todoTitle;
-
-    }
-});
-
-cancelEditBtn.addEventListener("click", (e)=> {
-    e.preventDefault();
-
-    toggleForms();
-});
-
-editForm.addEventListener("submit", (e)=> {
-    e.preventDefault();
-
-    const editInputValue = editInput.value;
-    if(editInputValue) {
-        updateTodo(editInputValue);
-    }
-
-    toggleForms();
 });
